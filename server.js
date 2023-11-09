54c5f79abd04f8b4447fd2fa27ac1824d48ccd61
Anilg1996 <you@example.com>
2023-11-09 17:03:00 +0530

    transaltor complete

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ app.use(express.json());
 
 
 app.post("/translate", async (req, res) => {
+    try {
     const { message, to } = req.body 
-   const translation =  await translate(message, { from: "auto",to: to })
-   return res.status(200).send({status: true, message: "Translate Your Language", data: translation})
+   const translation =  await translate(message, { from: "auto", to: to })
+   return res.status(200).send({status: true, message: "Translate Your Language", data: translation});
+   
+    } catch(err){
+        console.log(error);
+        return res.status(500).send({ status: false, error: error.message });
+    }
 })
 app.listen(process.env.PORT || 3000,  () => {
     console.log("Server is Running at Port....");
